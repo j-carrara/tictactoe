202c6ea4acfe2dbf98cca115039c3f2ce84ba53f
Joseph A Carrara <jac1109@wildcats.unh.edu>
2022-12-06 15:05:07 -0500

    initial tictactoe board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      boardState: [],
+      turn: 'x'
+    }
+    for (let x = 0; x < 9; x++) {
+      this.state.boardState.push('')
+    }
+  }
+
+  BoardSpaces() {
+    const rows = [];
+    for (let i = 0; i < 9; i++) {
+      rows.push(
+        <div className="playarea">
+          {this.BoardSpace(i)}
+        </div>
+      )
+    }
+    return rows;
+  }
+
+  BoardSpace(i) {
+    if(this.state.boardState[i] === 'x'){
+      return <div className='playspace'>x</div>
+    }
+    else if(this.state.boardState[i] === 'o'){
+      return <div className='playspace'>o</div>
+    }
+    else{
+      return <button className='playspace' style={{backgroundColor: this.state.boardState[i]}} onClick={() => this.move(i)}/>
+    }
+  }
+
+  move(i) {
+    const newBoardState = this.state.boardState.slice()
+    newBoardState[i] = this.state.turn === 'x' ? 'x' : 'o'
+    this.setState({turn: this.state.turn === 'x' ? 'o' : 'x', boardState: newBoardState})
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <div className="boardspace">
+          {this.BoardSpaces(this.state.boardState, (board) => {this.setState({boardState: board, turn: this.state.turn})})}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
